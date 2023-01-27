adee4bf01e1e380ff5cf91dd18afa54a26ecb546
ender1311 <120590237+ender1311@users.noreply.github.com>
2023-01-26 21:14:32 -0500

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+'use strict';
+
+const webpack = require('webpack'); // eslint-disable-line no-unused-vars
+
+module.exports = {
+  mode: 'production',
+  entry: './browser/index.js',
+  output: {
+    path: __dirname,
+    filename: './public/js/bundle.js',
+  },
+  context: __dirname,
+  devtool: 'source-map',
+  module: {
+    loaders: [
+      {
+        test: /jsx?$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['react', 'es2015', 'stage-2'],
+        }
+      }
+    ]
+  }
+};
